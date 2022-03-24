5364deb1a67cf1488b4aae05ec92b880127a556e
Manuel UOC <manuelgarciacr@uoc.edu>
2022-03-24 12:18:37 +0100

    Contact Wheel
     On branch contact-wheel
     Changes to be committed:
    	modified:   assets/css/custom.css
    	modified:   assets/js/contact.js
    	modified:   contact.html

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,26 @@
 let sections = [];
 let offsets = [];
+let wheeling;
 
+// Offsets
+//
+const calcOffsets = () => {
+    const offset0 = sections[0].offsetTop;
+    offsets = sections.map(el => el.offsetTop - offset0)
+}
+
+window.onresize = calcOffsets;
+//
+// End Offsets
+
+// Scroll
+//
 const setScroll = (n) => {
     const offset = sections[n].offsetTop - sections[0].offsetTop;
     window.scrollTo(window.scrollX, offset);
 }
 
-const showDiffs = () => {
+const setDiffs = () => {
     const y = window.scrollY;
     const diffs = offsets.map(ofs => Math.abs(y - ofs));
     const idx = diffs.indexOf(Math.min(...diffs));
@@ -16,26 +30,38 @@ const showDiffs = () => {
 }
 
 const onScroll = () => {
-    window.removeEventListener('mouseup', showDiffs, false);
-    window.addEventListener('mouseup', showDiffs, { one: true });
-    window.removeEventListener('touchend', showDiffs, false);
-    window.addEventListener('touchend', showDiffs, { one: true });
+    window.removeEventListener('mouseup', setDiffs, false);
+    window.addEventListener('mouseup', setDiffs, { one: true });
+    window.removeEventListener('touchend', setDiffs, false);
+    window.addEventListener('touchend', setDiffs, { one: true });
 };
 
-const calcOffsets = () => {
-    const offset0 = sections[0].offsetTop;
-    offsets = sections.map(el => el.offsetTop - offset0)
-    console.log("Offsets:", offsets)
-}
-
 window.onscroll = onScroll;
-window.onwheel = showDiffs;
-window.onresize = calcOffsets;
+//
+// End Scroll
+
+// Wheel
+//
+window.onwheel = (e) => {
+
+    clearTimeout(wheeling);
+
+    wheeling = setTimeout(() => {
+
+        wheeling = undefined;
+
+        setDiffs()
+    }, 250);
+};
+//
+// End Wheel
 
 window.addEventListener("DOMContentLoaded", function () {
     sections = [].slice.call(document.getElementsByTagName('section'));
     const footer = document.getElementsByTagName('footer')[0];
+    
     sections.push(footer);
+
     calcOffsets()
     
 }, false);
