8b8721d2935fd3fb9d26adb708ee623c5157a000
icardus <ignasi.cardus@gmail.com>
2022-04-07 13:32:35 +0200

    search mods
     Author:    icardus <ignasi.cardus@gmail.com>
    
     interactive rebase in progress; onto 66a0853
     Last commands done (5 commands done):
        pick 2096b2d varis dimarts5
        pick 8a13905 search mods
     No commands remaining.
     You are currently rebasing branch 'mixignasi3' on '66a0853'.
    
     Changes to be committed:
    	modified:   assets/js/search.js
    	deleted:    search.html
    	modified:   shop.html

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,39 +1,16 @@
-const search = document.querySelector('#search');
-const todos = document.querySelectorAll('ul li');
-const notFound = document.querySelector('#notFound');
+const search = document.querySelector("input#search");
+console.log("SS", search)
+function getProductsFromName(name) {
+  const filterName = name.toUpperCase();
+  const productesByName = products.filter(prod => {
+    const prodName = prod.name.toUpperCase();
+    return prodName.includes(filterName)
+  });
+  console.table(productesByName);
+  return productesByName;
+}
 
-search.addEventListener('keyup', filterFunctionality);
-
-function filterFunctionality(e) {
-    let searching = e.target.value.toLowerCase();
-  
-
-    [...todos].forEach(todo => {
-      let todoContent = todo.textContent;
-      if (todoContent.toLowerCase().includes(searching)) {
-        todo.style.display = 'block';
-      } else {
-        todo.style.display = 'none';
-      }
-    });
-  
-    // Mostrar que no s'ha trobat
-    let result = [...todos].every(todo => {
-      return todo.style.display === 'none';
-    });
-  
-    result === true
-      ? (notFound.style.display = 'block')
-      : (notFound.style.display = 'none');
-  
-  }
-
-    // Filtre tot
-    [...todos].forEach(todo => {
-        let todoContent = todo.textContent;
-        if (todoContent.toLowerCase().includes(searching)) {
-          todo.style.display = 'block';
-        } else {
-          todo.style.display = 'none';
-        }
-      });
+search.addEventListener("onKeyUp", ev => {
+  ev.preventDefault();
+  console.log(ev);
+}, true);
