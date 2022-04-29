ce810d17b54711ea6643f01876e25fa6ac65e979
icardus <ignasi.cardus@gmail.com>
2022-04-29 12:44:47 +0200

    ultim a classe

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,3 +1,10 @@
+/*CARGA*/
+<script type="text/javascript">
+    function popUp(URL) {
+        window.open(URL, 'Nombre de la ventana', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=300,height=200,left = 390,top = 50');
+    }
+    </script>
+
 /*POPUP*/
 const button = document.querySelector('#accedir');
 const popup = document.querySelector('.popup-wrapper');
